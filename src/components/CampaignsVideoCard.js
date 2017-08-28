fe34221345b78d719a90b2b6b76e5154de36f909
Eduardo Antuña Díez <edu@MacBook-Pro-de-Eduardo.local>
2017-08-28 23:28:03 +0200

    Use iframe instead of a thumbnail img

--- src/components/CampaignsVideoCard.js
+++ src/components/CampaignsVideoCard.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import YouTube from 'react-youtube'
 
 class CampaignsVideoCard extends Component {
     render() {
         const youtubeURLBase = "https://www.youtube.com/watch?v="
+        const opts = {
+            playerVars: { // https://developers.google.com/youtube/player_parameters
+                modestbranding: 1,
+                rel: 0,
+                playsinline: 1
+            }
+        }
         return (
             <div className="card">
-                <img className="card-img-top" src={this.props.thumbnail} alt="" />
+                {//<img className="card-img-top" src={this.props.thumbnail} alt="" />
+                }
+                <YouTube
+                className='youtubePlayer'
+                    videoId={this.props.videoId}       // defaults -> null
+                    opts={opts}                        // defaults -> {}
+                    onReady={this._onReady}
+                />
                 <div className="card-body">
                     <h4 className="card-title">{this.props.title}</h4>
                     <div className="card-text" dangerouslySetInnerHTML={{ __html: this.props.description }}></div>
@@ -17,6 +32,10 @@ class CampaignsVideoCard extends Component {
                 </div>
             </div>
         )
+    }
+
+    _onReady(event) {
+        // access to player in all event handlers via event.target
     }
 }
 
